1516fb2bc77f6a578adf40349faf847afbfb5c18
arif-ozberk <ozberkazmak@gmail.com>
2023-09-02 19:22:53 +0300

    first commit

---
+++ Members.js
@@ -0,0 +1,19 @@
+const userData = [
+    {
+        userName: "Arif",
+        age: 21,
+        job: "Engineer"
+    },
+    {
+        userName: "Memet",
+        age: 25,
+        job: "Chef"
+    },
+    {
+        userName: "Ahmet",
+        age: 30,
+        job: "Driver"
+    },
+];
+
+export default userData;
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+import moment from "moment";
+import userData from "./Members.js";
+
+const app = express();
+const PORT = 5000;
+
+
+const myMiddleware = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl} ${moment().format()}`);
+    next();
+}
+
+app.use(myMiddleware);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/data/users", (reg, res) => {
+    res.json(userData);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+});
